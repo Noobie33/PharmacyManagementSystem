17c7e337a328518fe6fe269e96d38cc70bdc235d
shakib-mozumder <shakb0090@gmail.com>
2026-01-22 14:57:30 +0600

    Add button in pharmacist Dashboard

--- PharmacyManagementSystem/PharmacistDashboard.Designer.cs
+++ PharmacyManagementSystem/PharmacistDashboard.Designer.cs
@@ -29,23 +29,38 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(303, 119);
+            this.label1.Location = new System.Drawing.Point(454, 183);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(114, 13);
+            this.label1.Size = new System.Drawing.Size(171, 20);
             this.label1.TabIndex = 1;
             this.label1.Text = "Pharmacist Dashboard";
             // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(379, 242);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(307, 47);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Inventory Management";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
             // PharmacistDashboard
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1200, 692);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "PharmacistDashboard";
             this.Text = "PharmacistDashboard";
             this.ResumeLayout(false);
@@ -56,5 +71,6 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
     }
 }
